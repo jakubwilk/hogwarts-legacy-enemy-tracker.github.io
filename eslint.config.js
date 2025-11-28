ec59017b25114a52698f87d7daac9604be18c104
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2025-11-28 10:47:12 +0100

    Add Prettier and ESLint configuration with Mantine and i18next support

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -14,10 +16,33 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier.configs.recommended,
+      simpleImportSort.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      strict: ['error', 'global'],
+      'prettier/prettier': 'error',
+      'react/jsx-curly-brace-presence': [2, 'always'],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Config from https://dev.to/julioxavierr/sorting-your-imports-with-eslint-3ped
+            ['^react', '^next', '^@?\\w'],
+            ['^(@|components)(/.*|$)'],
+            ['^\\u0000'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            ['^.+\\.?(css)$'],
+          ],
+        },
+      ],
     },
   },
 ])
---
+++ src/app/i18n.ts
@@ -0,0 +1,28 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import en from 'lang/en.json'
+import pl from 'lang/pl.json'
+
+const resources = {
+  en: {
+    translation: {
+      ...en,
+    },
+  },
+  pl: {
+    translation: {
+      ...pl,
+    },
+  },
+}
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+})
+
+export default i18n
