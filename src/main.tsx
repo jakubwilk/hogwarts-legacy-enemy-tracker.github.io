16c3b1162df7e86a8bbe70fcf510d32e59d8b353
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2025-11-28 10:47:26 +0100

    Enhance project configuration with TypeScript path aliases, integrate Mantine for UI theming, and add internationalization support with English and Polish language files.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import './app/i18n.ts'
+import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
+import { createTheme, MantineProvider } from '@mantine/core'
+
+const theme = createTheme({})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
